81575c78c0c34d966a58512a4e77a4f97c50890e
Yohei Katoh <esper0328@gmail.com>
2014-07-13 15:36:14 +0900

    Commit For Windows

--- TrickEstimater.cpp
+++ TrickEstimater.cpp
@@ -7,16 +7,19 @@
 //
 
 #include "TrickEstimater.h"
+#include <fstream>
 
 const unsigned char c_integral_thresh = 175;
 
 bool is_sharp_angle[NUMBER_OF_HANDS][NUMBER_OF_CONTOUR_COORDINATE] = { false };
 unsigned short label_index[NUMBER_OF_HANDS][NUMBER_OF_CONTOUR_COORDINATE] = { 0 };
 unsigned short label_index_max = 0;
+std::ofstream* the_output_file = NULL;
 
 TrickEstimater::TrickEstimater()
 {
     initialize();
+	the_output_file = new std::ofstream("result.txt");
 }
 
 TrickEstimater::~TrickEstimater()
@@ -64,11 +67,11 @@ double TrickEstimater::calcOuterProduct(double rv_x1, double rv_y1, double rv_x2
 void TrickEstimater::labelContour(unsigned int contour_id, unsigned int rv_size)
 {
 	LabelingBS	labeling;
-	bool at_is_sort_size_order = false;//óÃàÊÇÃëÂÇ´Ç≥èáÇ…É\Å[ÉgÇµÇ»Ç¢
-	int at_delete_size = 0;    //è¡ãéÇ∑ÇÈè¨óÃàÊÇÃç≈ëÂÉTÉCÉY(int) - Ç±ÇÍà»â∫ÇÃÉTÉCÉYÇÃóÃàÊÇè¡ãéÇ∑ÇÈ
+	bool at_is_sort_size_order = false;
+	int at_delete_size = 0;
 	unsigned char *src = new unsigned char[rv_size];
 	short *result = new short[rv_size];
-    
+
 	for (int i = 0; i < rv_size; i++){
 		src[i] = (unsigned char)is_sharp_angle[contour_id][i];
 	}
@@ -125,27 +128,25 @@ void TrickEstimater::calcContourFeature()
         std::cerr << contours[i].size() << std::endl;
 		if (contours[i].size() >  2 * interval){
 			for (int j = 0; j < contours[i].size() - 2 * interval; j++){
-				//ó÷äsÇÃâsäpÇåvéZ
 				norm_a = calcNorm(contours[i][j].x, contours[i][j].y, contours[i][j + interval].x, contours[i][j + interval].y);
 				norm_b = calcNorm(contours[i][j + 2 * interval].x, contours[i][j + 2 * interval].y, contours[i][j + interval].x, contours[i][j + interval].y);
 				inner_product_ab = calcInnerProduct(contours[i][j].x, contours[i][j].y, contours[i][j + interval].x, contours[i][j + interval].y, contours[i][j + 2 * interval].x, contours[i][j + 2 * interval].y);
 				theta = acos(inner_product_ab / (norm_a * norm_b));
 				outer_product_ab = calcOuterProduct(contours[i][j].x, contours[i][j].y, contours[i][j + interval].x, contours[i][j + interval].y, contours[i][j + 2 * interval].x, contours[i][j + 2 * interval].y);
 				is_sharp_angle[i][j + interval] = isSharpAngle(theta);
-				//*the_output_file << contours[i][j].x << "\t" << contours[i][j].y << "\t" << norm_a << "\t" << norm_b << "\t" << inner_product_ab << "\t" << theta << "\t" << outer_product_ab << std::endl;
+				*the_output_file << contours[i][j].x << "\t" << contours[i][j].y << "\t" << norm_a << "\t" << norm_b << "\t" << inner_product_ab << "\t" << theta << "\t" << outer_product_ab << std::endl;
 			}
-			//ó÷äsÇÃâsäpóÃàÊÇÃÉâÉxÉãêîÇì¡í•ó Ç∆ÇµÇƒéZèo
 			contour_id[k] = i;
 			labelContour(contour_id[k], contours[i].size());
 			k++;
-			//*the_output_file << label_index_max << std::endl;
+			*the_output_file << label_index_max << std::endl;
 		}
 	}
 }
 
 bool TrickEstimater::isSharpAngle(const double rv_theta)
 {
-	double sharp_angle_thresh = 1.04719;//[rad]:60deg
+	double sharp_angle_thresh = 1.4;//[rad]:60deg:1.04719
 	if (rv_theta < sharp_angle_thresh){
 		//*the_output_file << contours[i][j + interval].x << "\t" << contours[i][j + interval].y << "\t" << theta << "\t" << outer_product_ab << std::endl;
 		return true;
@@ -157,20 +158,19 @@ bool TrickEstimater::isSharpAngle(const double rv_theta)
 
 EHAND TrickEstimater::judgeScissorsPaperRock(const cv::Mat& rv_src_img)
 {
-	//2ílâª
+
     cv::Mat mask_img;
 	cv::threshold(rv_src_img, mask_img, c_integral_thresh, 255, THRESH_BINARY);
 	
-	//ó÷äsíäèo
 	vector<Vec4i> hierarchy;
     contours.clear();
 	findContours(mask_img, contours, hierarchy,	CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
 	
 	calcContourFeature();
     
-	if(label_index_max > 0 && label_index_max  < 3 )return EHAND::eHAND_ROCK;
-	else if(label_index_max > 2 && label_index_max < 5)return EHAND::eHAND_SCISSORS;
-	else if(label_index_max > 6)return EHAND::eHAND_PAPER;
+	if(label_index_max  == 1 || label_index_max  == 2)return EHAND::eHAND_ROCK;
+	else if(label_index_max == 3 || label_index_max == 4)return EHAND::eHAND_SCISSORS;
+	else if(label_index_max > 4)return EHAND::eHAND_PAPER;
 	else return EHAND::eHAND_ERROR;
 }
 
